d3527ab21b6c6a32eb8a52bc805d22fd1387080b
KfirShainholtz <kfir.s@logz.io>
2018-08-07 18:24:32 +0300

    Some $$$

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # grafana-http-client
 An unofficial grafana http client for javascript applications.
+
+## Tests:
+(TODO): The integration tests are using a grafana server instance to validate
+the current API.
+
+Nothing is implemented yet! TBD A$AP
---
+++ index.ts
@@ -0,0 +1,22 @@
+import Admin from './lib/public/api/admin';
+import Alerting from './lib/public/api/alerting';
+import Users from './lib/public/api/users';
+import annotations from './lib/public/api/annotations';
+import authentication from './lib/public/api/authentication';
+import dashboardPermissions from './lib/public/api/dashboard-permissions';
+import dashboardVersions from './lib/public/api/dashboard-versions';
+import dashboard from './lib/public/api/dashboard';
+import folderPermissions from './lib/public/api/folder-permissions';
+import misc from './lib/public/api/misc';
+import organisation from './lib/public/api/organisation';
+import preferences from './lib/public/api/preferences';
+import snapshot from './lib/public/api/snapshot';
+import teams from './lib/public/api/teams';
+
+import GrafanaClient from './lib/public/client/grafana-client';
+
+export const grafanaAdmin = Admin;
+export const grafanaAlerting = Alerting;
+export const grafanaUsers = Users;
+
+export default GrafanaClient;
